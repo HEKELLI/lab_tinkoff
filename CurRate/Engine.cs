8cb97db1ff28d2ef4c84e41d4de797cfe1b036fc
HEKELLI <51367205+HEKELLI@users.noreply.github.com>
2021-05-02 00:58:55 +0700

    Реализовал интерфейс портфолио
    Теперь пользователь может просматривать бумаги, которые находятся в его портфеле. А так же пользователь имеет возможность проверить текущий баланс.

--- CurRate/Engine.cs
+++ CurRate/Engine.cs
@@ -14,6 +14,13 @@ namespace CurRate
         public Tinkoff.Trading.OpenApi.Models.MarketInstrumentList Securities_stocks;
         public Tinkoff.Trading.OpenApi.Models.MarketInstrumentList Securities_bonds;
         public Tinkoff.Trading.OpenApi.Models.MarketInstrumentList Securities_etfs;
+        //private Tinkoff.Trading.OpenApi.Models.SandboxAccount Account;
+        //private Tinkoff.Trading.OpenApi.Models.BrokerAccountType type;
+        private Tinkoff.Trading.OpenApi.Models.Portfolio portfolio;
+        private Tinkoff.Trading.OpenApi.Models.PortfolioCurrencies currencies;
+        private string brokerAccountId = "SB3336849";
+        int tmp = 0;
+        private bool flag = false;
         public void get_connection(string token)
         {
             connection = ConnectionFactory.GetSandboxConnection(token);
@@ -21,7 +28,58 @@ namespace CurRate
             get_stocks_async();
             get_bonds_async();
             get_etfs_async();
+            get_porfolio_async();
+            get_currencies_async();
+            //register();
         }
+        /*
+        public async void register()
+        {
+            //type = Account.BrokerAccountType;
+            Account = await context.RegisterAsync(type);
+            brokerAccountId = Account.BrokerAccountId;
+        }
+        */
+        public Tinkoff.Trading.OpenApi.Models.PortfolioCurrencies get_currencies()
+        {
+            get_currencies_async();
+            if (flag == true)
+            {
+                flag = false;
+                return currencies;
+            }
+            else
+            {
+                while(flag==false)
+                {
+                    tmp++;
+                }
+                tmp = 0;
+                return currencies;
+            }
+        }
+
+        private async void get_currencies_async()
+        {
+            currencies = await context.PortfolioCurrenciesAsync();
+            flag = true;
+        }
+
+        public Tinkoff.Trading.OpenApi.Models.Portfolio get_portfolio()
+        {
+            get_porfolio_async();
+            if (flag == true)
+                return portfolio;
+            else
+                return null;
+        }
+
+        private async void get_porfolio_async()
+        {
+            portfolio = await context.PortfolioAsync(brokerAccountId);
+            flag = true;
+        }
+
         private async void get_stocks_async()
         {
             Securities_stocks = await context.MarketStocksAsync();
--- CurRate/PortfolioForm.cs
+++ CurRate/PortfolioForm.cs
@@ -9,30 +9,117 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.Json;
 using Tinkoff.Trading.OpenApi.Network;
+using MaterialSkin.Controls;
 
 namespace CurRate
 {
-    public partial class PortfolioForm : Form
+    public partial class PortfolioForm : MaterialForm
     {
         public PortfolioForm(MainForm mainForm)
         {
             InitializeComponent();
             this.mainForm = mainForm;
             get_portfolio();
+            get_currencies();
+            start_form_and_write_position();
         }
         MainForm mainForm;
+        private Tinkoff.Trading.OpenApi.Models.Portfolio portfolio;
+        private Tinkoff.Trading.OpenApi.Models.PortfolioCurrencies currencies;
+        private int i = 0;
         private void but_main_form_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-        private async void get_portfolio()
+        private void get_currencies()
         {
+            currencies = mainForm.engine.get_currencies();
+        }
+        private void get_portfolio()
+        {
+           portfolio = mainForm.engine.get_portfolio();
           //  mainForm.portfolio = await mainForm.context.PortfolioAsync("SB3336849");
             write_portfolio();
         }
         public void write_portfolio()
         {
-           // textBox1.Text = JsonSerializer.Serialize<Tinkoff.Trading.OpenApi.Models.Portfolio>(mainForm.portfolio);
+            textBox1.Text = JsonSerializer.Serialize<Tinkoff.Trading.OpenApi.Models.Portfolio>(portfolio);
+        }
+
+        private void but_show_rub_Click(object sender, EventArgs e)
+        {
+            get_currencies();
+            materialLabel1.Text = "Баланс в рублях: " + search_currency("Rub");
+        }
+        private string search_currency(string cur)
+        {
+            for(int i=0; i<currencies.Currencies.Count;i++)
+            {
+                if(Convert.ToString(currencies.Currencies[i].Currency) == cur)
+                {
+                    return Convert.ToString(currencies.Currencies[i].Balance);
+                }
+            }
+            return null;
+        }
+
+        private void but_show_eur_Click(object sender, EventArgs e)
+        {
+            get_currencies();
+            materialLabel1.Text = "Баланс в евро: " + search_currency("Eur");
+        }
+
+        private void but_show_usd_Click(object sender, EventArgs e)
+        {
+            get_currencies();
+            materialLabel1.Text = "Баланс в долларах: " + search_currency("Usd");
+        }
+
+        private void write_position(Tinkoff.Trading.OpenApi.Models.Portfolio.Position position)
+        {
+            materialSingleLineTextField1.Text = position.Figi;
+            materialSingleLineTextField2.Text = position.Ticker;
+            materialSingleLineTextField3.Text = position.Isin;
+            materialSingleLineTextField4.Text = Convert.ToString(position.InstrumentType);
+            materialSingleLineTextField5.Text = Convert.ToString(position.Balance);
+            materialSingleLineTextField6.Text = Convert.ToString(position.Blocked);
+            materialSingleLineTextField7.Text = Convert.ToString(position.Lots);
+            materialSingleLineTextField8.Text = position.Name;
+        }
+
+        private void start_form_and_write_position()
+        {
+            if (portfolio.Positions[i] != null)
+            {
+                if (Convert.ToString(portfolio.Positions[i].InstrumentType) != "Currency")
+                {
+                    write_position(portfolio.Positions[i]);
+                }
+            }
+        }
+
+        private void next_pos_Click(object sender, EventArgs e)
+        {
+            if (i<portfolio.Positions.Count)
+            {
+                if (Convert.ToString(portfolio.Positions[i+1].InstrumentType) != "Currency")
+                {
+                    i++;
+                    write_position(portfolio.Positions[i]);
+                }
+            }
+        }
+
+        private void prev_pos_Click(object sender, EventArgs e)
+        {
+            if (portfolio.Positions[i] != null)
+            {
+                if (i>0)
+                {
+                    i--;
+                    write_position(portfolio.Positions[i]);
+                }
+            }
         }
     }
 }
